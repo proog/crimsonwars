76e8e3b3bec0f46f0e9344cac73f742b5d500b8e
Per Mortensen <per@permortensen.com>
2021-10-08 21:50:47 +0200

    Upgrade to 11ty 1.0 beta

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,4 @@
 module.exports = function (eleventyConfig) {
-  const { Liquid } = require("liquidjs");
-  const engine = new Liquid({
-    extname: ".html",
-    dynamicPartials: true,
-    strict_filters: true,
-    root: ["_includes"],
-  });
-
-  eleventyConfig.setLibrary("liquid", engine);
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("favicon.ico");
 };
