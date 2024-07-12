acfeeeb56de2c795a3162975aa2954135e4a864b
Per Mortensen <per@permortensen.com>
2024-07-12 20:50:58 +0200

    Remove Bootstrap CDN dependency

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/bootstrap/dist/css/bootstrap.min.css":
+      "assets/css/bootstrap.min.css",
+    "node_modules/bootstrap/dist/js/bootstrap.min.js":
+      "assets/js/bootstrap.min.js",
+    "node_modules/jquery/dist/jquery.slim.min.js":
+      "assets/js/jquery.slim.min.js",
+    "node_modules/popper.js/dist/umd/popper.min.js": "assets/js/popper.min.js",
+  });
 };
